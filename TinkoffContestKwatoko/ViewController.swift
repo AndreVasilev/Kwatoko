8145e13835a063636da895afcadf23639b6dca63
Andrey Vasilev <ao.vasilev@gmail.com>
2022-05-16 10:56:15 +0300

    Initial Commit

---
+++ TinkoffContestKwatoko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffContestKwatoko
+//
+//  Created by Andrey Vasilev on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
